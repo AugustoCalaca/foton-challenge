c87f289d394cb92ccfc19773c2223f1e2ab08d2e
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-03 18:11:23 -0300

    change proportions and color fab

--- frontend/src/components/Fab.js
+++ frontend/src/components/Fab.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import styled from 'styled-components/native';
 
 const StyledTouchable = styled.TouchableOpacity`
-  right: 20;
-  bottom: 20;
-  width: 60;
-  height: 60;
+  right: 15;
+  bottom: 15;
+  width: 55;
+  height: 55;
   border-radius:  50;
   position: absolute;
   align-items: center;
   justify-content: center;
-  background-color: #1D1661;
+  background-color: #3227AB;
 `;
 
 const Fab = ({ navigation }) => {
@@ -21,7 +20,7 @@ const Fab = ({ navigation }) => {
       activeOpacity={0.7}
       onPress={_ => navigation.navigate('Create')}
     >
-      <Icon name='plus'  size={25} color='#fff' />
+      <Icon name='plus' size={20} color='#fff' />
     </StyledTouchable>
   )
 };
