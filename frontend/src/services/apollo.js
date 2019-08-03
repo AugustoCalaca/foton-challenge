d0d18c326636479a7bfb778ac0eacbcbbb21b692
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-03 18:16:11 -0300

    add web socket link

--- frontend/src/services/apollo.js
+++ frontend/src/services/apollo.js
@@ -1,12 +1,13 @@
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { split } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
-import { getToken, signin, signout } from '../utils/auth';
+import { getToken } from '../utils/auth';
 
-const httpLink = new HttpLink({
-  uri: 'https://foton-server.herokuapp.com/graphql',
-});
+const URL = 'foton-server.herokuapp.com/graphql';
 
 const authLink = setContext(async (req, { headers }) => {
   const token = await getToken();
@@ -19,9 +20,45 @@ const authLink = setContext(async (req, { headers }) => {
   };
 });
 
+const httpLink = new HttpLink({
+  uri: `https://${URL}`
+});
+
+const httpWithAuthToken = authLink.concat(httpLink);
+
+const wsLink = new WebSocketLink({
+  uri: `wss://${URL}`,
+  options: {
+    reconnect: true,
+    connectionParams: async _ => {
+      const token = await getToken();
+      return {
+        headers: {
+          authorization: token ? `Bearer ${token}` : null
+        }
+      }
+    },
+  }
+});
+
+const isSubscriptionOperation = ({ query }) => {
+  const { kind, operation } = getMainDefinition(query);
+  return (
+    kind === 'OperationDefinition' && operation === 'subscription'
+  );
+};
+
+const link = split(
+  isSubscriptionOperation,
+  wsLink,
+  httpWithAuthToken,
+);
+
+const cache = new InMemoryCache();
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  link,
+  cache
 });
 
 export default client;
