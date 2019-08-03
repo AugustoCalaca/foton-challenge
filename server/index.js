4f5d394ff7284e2ae0c50e59be661d5bc6b4a526
AugustoCalaca <augustocalaca2@gmail.com>
2019-08-03 16:31:29 -0300

    add http server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,26 @@
-const { ApolloServer, gql } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
+const { ApolloServer } = require('apollo-server-express');
+const { createServer } = require('http');
 const express = require('express');
+
 const connect = require('./src/services/database');
 const schema = require('./src/utils/schema');
 const context = require('./src/utils/context');
 
 connect();
+const PORT = process.env.PORT || 4000;
 
-const server = new ApolloServer({
+const apolloServer = new ApolloServer({
   schema,
   context
 });
 
 const app = express();
-server.applyMiddleware({ app });
+apolloServer.applyMiddleware({ app });
 
-app.listen({port: process.env.PORT || 4000}, _ => {
-  console.log(`Server ready at localhost:port${server.graphqlPath}`);
+const httpServer = createServer(app);
+apolloServer.installSubscriptionHandlers(httpServer);
+
+httpServer.listen({port: PORT}, _ => {
+  console.log(`Server ready at localhost:${PORT}${apolloServer.graphqlPath}`);
+  console.log(`Subscriptions ready at ws://localhost:${PORT}${apolloServer.subscriptionsPath}`);
 });
